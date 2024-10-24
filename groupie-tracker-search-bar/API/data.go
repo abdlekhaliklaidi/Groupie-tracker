3ce383ad4aa37be3a1e3123ad990011852935fa0
alaidi <abdlekhaliklaidi@gmail.com>
2024-10-24 13:43:28 +0100

    groupie tracker search

---
+++ groupie-tracker-search-bar/API/data.go
@@ -0,0 +1,40 @@
+package apiserver
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type ApiData struct {
+	BandImages    []string            `json:"BandImages"`
+	MembersImages []string            `json:"MembersImages"`
+	YoutubeLinks  map[string][]string `json:"YoutubeLinks"`
+}
+
+// Initialise my ApiData struct fields.
+func (data *ApiData) Initialise() {
+	d, err := LoadDataFromFile("./API/apidata.json")
+	if err != nil {
+		log.Fatalf("Error loading data: %v", err)
+	}
+
+	data.BandImages = d.BandImages
+	data.MembersImages = d.MembersImages
+	data.YoutubeLinks = d.YoutubeLinks
+}
+
+func LoadDataFromFile(filename string) (*ApiData, error) {
+	fileContent, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var data ApiData
+	err = json.Unmarshal(fileContent, &data)
+	if err != nil {
+		return nil, err
+	}
+
+	return &data, nil
+}
